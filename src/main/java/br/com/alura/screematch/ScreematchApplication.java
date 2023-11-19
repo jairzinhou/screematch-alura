9fdcd1b3395cc197296911e1bb1d685661a31cfd
jairzinhou <jairaraujo209@gmail.com>
2023-11-19 20:19:29 -0300

    m

---
+++ src/main/java/br/com/alura/screematch/ScreematchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screematch;
+
+import br.com.alura.screematch.model.DadosSerie;
+import br.com.alura.screematch.service.ConsumoApi;
+import br.com.alura.screematch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreematchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreematchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String endereco = "https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c";
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados(endereco);
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie.avaliacao());
+	}
+}
